2f66fc0f46af194247b501b30a21925122f7bc89
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-06-01 11:42:21 +0200

    typo

--- src/com/jantvrdik/intellij/latte/editorActions/LatteTypedHandler.java
+++ src/com/jantvrdik/intellij/latte/editorActions/LatteTypedHandler.java
@@ -8,18 +8,18 @@ import com.jantvrdik.intellij.latte.LatteLanguage;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Auto-inserts '}' after typing '{'.
+ * Handles individual keystrokes.
  */
 public class LatteTypedHandler extends TypedHandlerDelegate {
 	@Override
 	public Result charTyped(char charTyped, Project project, @NotNull Editor editor, @NotNull PsiFile file) {
+		// auto-inserts '}' after typing '{'
 		if (charTyped == '{' && project != null && file.getLanguage() == LatteLanguage.INSTANCE) {
 			int offset = editor.getCaretModel().getOffset();
 			editor.getDocument().insertString(offset, "}");
 			return Result.STOP;
-
-		} else {
-			return Result.CONTINUE;
 		}
+
+		return super.charTyped(charTyped, project, editor, file);
 	}
 }
