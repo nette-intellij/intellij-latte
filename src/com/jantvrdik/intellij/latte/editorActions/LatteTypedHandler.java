6bed527c69b7271da3ca058bfc04519046698ec2
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-06-01 11:45:34 +0200

    LatteTypedHandler: typing '}' is ignored when the next character is '}'

--- src/com/jantvrdik/intellij/latte/editorActions/LatteTypedHandler.java
+++ src/com/jantvrdik/intellij/latte/editorActions/LatteTypedHandler.java
@@ -1,7 +1,9 @@
 package com.jantvrdik.intellij.latte.editorActions;
 
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
+import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
 import com.jantvrdik.intellij.latte.LatteLanguage;
@@ -11,6 +13,22 @@ import org.jetbrains.annotations.NotNull;
  * Handles individual keystrokes.
  */
 public class LatteTypedHandler extends TypedHandlerDelegate {
+	@Override
+	public Result beforeCharTyped(char charTyped, Project project, Editor editor, PsiFile file, FileType fileType) {
+		// ignores typing '}' before '}'
+		if (charTyped == '}' && project != null && file.getLanguage() == LatteLanguage.INSTANCE) {
+			CaretModel caretModel = editor.getCaretModel();
+			String text = editor.getDocument().getText();
+			int offset = caretModel.getOffset();
+			if (text.length() > offset && text.charAt(offset) == '}') {
+				caretModel.moveToOffset(offset + 1);
+				return Result.STOP;
+			}
+		}
+
+		return super.beforeCharTyped(charTyped, project, editor, file, fileType);
+	}
+
 	@Override
 	public Result charTyped(char charTyped, Project project, @NotNull Editor editor, @NotNull PsiFile file) {
 		// auto-inserts '}' after typing '{'
