6e4358e130a11eb60f992fbb07fac3675d30cce2
Jan Galek <admin@gcore.cz>
2018-05-14 11:17:05 +0200

    Added: macro spaceless

--- src/com/jantvrdik/intellij/latte/config/LatteConfiguration.java
+++ src/com/jantvrdik/intellij/latte/config/LatteConfiguration.java
@@ -43,6 +43,7 @@ public class LatteConfiguration {
 
 		addStandardMacro("switch", PAIR);
 		addStandardMacro("case", UNPAIRED);
+		addStandardMacro("spaceless", PAIR);
 
 		addStandardMacro("foreach", PAIR);
 		addStandardMacro("for", PAIR);
